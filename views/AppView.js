359d2812531cafb5f96f5a39597931e805c7e7e9
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-05-21 13:39:12 -0400

    Adding functionality to submit reviews

--- views/AppView.js
+++ views/AppView.js
@@ -2,11 +2,11 @@ var AppView = Backbone.View.extend({
   el: $('body'),
 
   events: {
-  'click .submit-beer': 'createBeer',
-  'click .view-beer': 'viewBeer'
-},
+    'click .submit-beer': 'createBeer',
+    'click .view-beer': 'viewBeer'
+  },
 
-  initialize: function () {
+  initialize: function() {
     this.$nameInput = this.$('#name-input');
     this.$styleInput = this.$('#style-input');
     this.$abvInput = this.$('#abv-input');
@@ -19,14 +19,18 @@ var AppView = Backbone.View.extend({
     this.listenTo(this.model, 'change:show_reviews', this.renderPage);
 
     this.renderBeers();
+
+    this.detailView = null;
+
+    this.listenTo(this.model, 'change:current_beer', this.renderDetailView);
   },
 
-  renderPage: function () {
+  renderPage: function() {
     this.$('.reviews-container').toggleClass('show', this.model.get('show_reviews'));
     this.$('.beers-container').toggleClass('show', !this.model.get('show_reviews'));
   },
 
-  createBeer: function () {
+  createBeer: function() {
     this.model.get('beers').add({
       name: this.$nameInput.val(),
       style: this.$styleInput.val(),
@@ -35,20 +39,32 @@ var AppView = Backbone.View.extend({
     });
   },
 
-  renderBeer: function (beer) {
-    var beerView = new BeerView({ model: beer });
+  renderBeer: function(beer) {
+    var beerView = new BeerView({
+      model: beer
+    });
     this.$beerList.append(beerView.render().el);
   },
 
-  renderBeers: function () {
-    this.model.get('beers').each(function (m) {
+  renderBeers: function() {
+    this.model.get('beers').each(function(m) {
       this.renderBeer(m);
     }, this);
   },
 
-  viewBeer: function (e) {
-  var clickedBeerId = $(e.currentTarget).data().id;
+  viewBeer: function(e) {
+    var clickedBeerId = $(e.currentTarget).data().id;
 
-  this.model.showReviews(clickedBeerId);
+    this.model.showReviews(clickedBeerId);
+  },
+
+  renderDetailView: function () {
+  if (this.detailView) {
+    this.detailView.remove();
+  }
+
+  this.detailView = new BeerDetailView({ model: this.model.get('current_beer')});
+
+  this.$('.reviews-container').append(this.detailView.render().el);
 }
 });
---
+++ views/BeerDetailView.js
@@ -0,0 +1,40 @@
+var BeerDetailView = Backbone.View.extend({
+  className: 'reviews-container-inner',
+
+  template: Handlebars.compile($('#beer-detail-template').html()),
+
+  initialize: function() {
+    this.listenTo(this.model.get('reviews'), 'add', this.renderReview);
+  },
+
+  events: {
+    'click .submit-review': 'createReview'
+  },
+
+  render: function () {
+  this.$el.html(this.template(this.model.toJSON()));
+
+  this.renderReviews();
+
+  return this;
+},
+
+renderReviews: function () {
+  this.model.get('reviews').each(function (m) {
+    this.renderReview(m);
+  }, this);
+},
+
+renderReview: function (review) {
+  var reviewView = new ReviewView({ model: review });
+  this.$('.reviews-list').append(reviewView.render().el);
+},
+
+createReview: function () {
+  this.model.get('reviews').add({
+    name: this.$('#review-name-input').val(),
+    text: this.$('#review-notes-input').val()
+  }, this);
+}
+
+});
